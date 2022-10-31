c995bbb36d54da5618d2bcb504db24a4a343cb7c
karthikeyanbaburajendran <61476328+undefined@users.noreply.github.com>
2022-10-31 13:00:37 +0000

    Push from TwoWayBinding

--- src/app.js
+++ src/app.js
@@ -2,4 +2,12 @@ import { LightningElement } from "lwc";
 
 export default class App extends LightningElement {
 fullname="Karthikeyan Baburajendran";
+title="";
+
+handlechange(event){
+  this.title=event.target.value;
+}
+handleClick(event){
+  this.fullname=this.title;
+}
 }
