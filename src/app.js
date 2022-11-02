2d69947d02cc80b1b0b1e389d4571a51ae292696
karthikeyanbaburajendran <61476328+undefined@users.noreply.github.com>
2022-11-02 13:10:16 +0000

    Push from forEachLoopDemo

--- src/app.js
+++ src/app.js
@@ -1,5 +1,30 @@
 import { LightningElement } from "lwc";
-
-export default class App extends LightningElement {
-fullname="Karthikeyan Baburajendran";
+export default class TemplateLoopingForEach extends LightningElement {
+  carList = ["Ford", "Audi", "Maruti", "Hyundai", "Mercedes"];
+  programmingList = [
+    {
+      id: "06868",
+      language: "HTML"
+    },
+    {
+      id: "19797",
+      language: "CSS"
+    },
+    {
+      id: "298789",
+      language: "Javascript"
+    },
+    {
+      id: "398798",
+      language: "Apex"
+    },
+    {
+      id: "48967",
+      language: "Aura"
+    },
+    {
+      id: "58798",
+      language: "Java"
+    }
+  ];
 }
