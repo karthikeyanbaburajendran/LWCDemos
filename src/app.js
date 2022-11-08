185c65dce2f92b7d5b044325cbeefd0c75eeb91e
karthikeyanbaburajendran <61476328+undefined@users.noreply.github.com>
2022-11-08 11:44:26 +0000

    Push from ConditionalRenderingifFalse

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import { LightningElement } from "lwc";
 export default class App extends LightningElement {
 fullname;
 details
-ShowText=false;
+hideText=false
 
 handleClick(){
   this.ShowText=!this.ShowText
   this.fullname="Name : karthikeyan"
   this.details="Designation: Software Engineer"
+}
+hideHandler(){
+  
+this.hideText=true;
 }
 }
