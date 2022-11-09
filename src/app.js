05243a593a21a3a6a10ce1c0140a1b7d8abc35d6
karthikeyanbaburajendran <61476328+undefined@users.noreply.github.com>
2022-11-09 12:03:00 +0000

    Push from ConditionalRenderingusingEventTriggerandgetterclass

--- src/app.js
+++ src/app.js
@@ -1,25 +1,16 @@
 import { LightningElement } from "lwc";
 
 export default class App extends LightningElement {
-fullname;
-details
-ShowText=false
-title="This is a getter class";
-sldbutton="OFF";
 
-handleClick(){
-  this.ShowText=!this.ShowText
-  this.fullname="Name : karthikeyan"
-  this.details="Designation: Software Engineer"
+name
+fullname
+storetheName(event){
+this.name=event.target.value
 }
-get getTitle(){
-  return this.title.toUpperCase();
+get showText(){
+  return this.fullname==="Karthikeyan"?true:false
 }
-selectButton(event){
-console.log(event.target.value)
-this.sldbutton=event.target.value;
-}
-get boxStatus(){
-  return `box ${this.sldbutton === 'ON' ? 'green':'red'}`
+onSubmit(event){
+  this.fullname=this.name
 }
 }
