fd8b31045a1a85122e19a5d58a33d5cff18daf05
karthikeyanbaburajendran <61476328+undefined@users.noreply.github.com>
2022-11-09 09:55:08 +0000

    Push from getterClassDemo

--- src/app.js
+++ src/app.js
@@ -3,11 +3,23 @@ import { LightningElement } from "lwc";
 export default class App extends LightningElement {
 fullname;
 details
-ShowText=false;
+ShowText=false
+title="This is a getter class";
+sldbutton="OFF";
 
 handleClick(){
   this.ShowText=!this.ShowText
   this.fullname="Name : karthikeyan"
   this.details="Designation: Software Engineer"
+}
+get getTitle(){
+  return this.title.toUpperCase();
+}
+get boxStatus(){
+  return `box ${this.sldbutton === 'ON' ? 'green':'red'}`
+}
+selectButton(event){
+//console.log(event.currentTarget.innerText)
+this.sldbutton=event.currentTarget.innerText;
 }
 }
