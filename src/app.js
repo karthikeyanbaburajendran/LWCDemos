7fc0f6156f724010021ce610199acd6a0d1e76bd
karthikeyanbaburajendran <61476328+undefined@users.noreply.github.com>
2022-11-14 11:02:36 +0000

    Push from multipleTemplateRendering

--- src/app.js
+++ src/app.js
@@ -1,5 +1,19 @@
-import { LightningElement } from "lwc";
+import { LightningElement } from 'lwc';
+import signinTemplate from './signinTemplate.html'
+import signupTemplate from './signupTemplate.html'
+import defaultTemplate from './app.html'
+export default class RenderMultipleDemo extends LightningElement {
 
-export default class App extends LightningElement {
-fullname="Karthikeyan Baburajendran";
+selectedTemplate
+
+render(){
+  return this.selectedTemplate==="Sign In"?signinTemplate:this.selectedTemplate==="Sign Up"?
+  
+  signupTemplate:defaultTemplate
+
+}
+
+handleClick(event){
+  this.selectedTemplate=event.target.value
+}
 }
