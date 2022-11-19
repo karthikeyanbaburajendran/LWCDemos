6f34816bcc37b53d0e4bf21a0880f95cf39a271f
karthikeyanbaburajendran <61476328+undefined@users.noreply.github.com>
2022-11-19 12:28:05 +0000

    Push from parentToChildCommunicationversion2

--- src/parentComponent.js
+++ src/parentComponent.js
@@ -2,11 +2,24 @@ import { LightningElement,track} from 'lwc';
 export default class parentComponent extends LightningElement {
 
 boo=false
-buttonLabel=true
-iter
-handleClick(){
-  this.buttonLabel=!this.buttonLabel
+buttonLabel="Show Child Component"
+buttonValue="Show"
+buttonVariant="success"
+
+handleClick(event){
+this.buttonValue=event.target.value
+if(this.buttonValue=="Show"){
+  this.buttonLabel="Hide Child Component"
   this.boo=!this.boo
-  console.log(this.buttonLabel)
+  this.buttonVariant="destructive"
+  this.buttonValue="Hide"
+}
+else{
+  this.buttonLabel="Show Child Component"
+  this.boo=!this.boo
+  this.buttonVariant="success"
+  this.buttonValue="Show"
+}
+
 }
 }
