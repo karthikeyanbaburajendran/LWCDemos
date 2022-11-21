9577c42a76d23e0f15b3a8615a69d6236ecf01e2
karthikeyanbaburajendran <61476328+undefined@users.noreply.github.com>
2022-11-21 15:28:46 +0000

    Push from parenttoChildCommunicationInArrays

--- src/parentComponent.js
+++ src/parentComponent.js
@@ -1,16 +1,39 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,api } from 'lwc';
 
-export default class MethodCaller extends LightningElement {
-  buttonAction=true
-    video = "https://www.w3schools.com/tags/movie.mp4";
+export default class AlertParentComponent extends LightningElement {
 
-    handlePlay() {
-        this.template.querySelector('c-child-component').play();//We call methods in child class here
-        this.buttonAction=!this.buttonAction
-    }
-
-    handlePause() {
-        this.template.querySelector('c-child-component').pause();//We call methods in child class here
-        this.buttonAction=!this.buttonAction
-    }
+ userDetails=[{
+            name: 'John Doe',
+            title: 'CEO & Founder',
+            company: 'Harvard University, example',
+            buttontext: 'Contact',
+            imageUrl: 'https://www.w3schools.com/w3images/team1.jpg',
+            phoneNumber:"9876578654"
+        },
+        {
+            name: 'Steve Smith',
+            title: 'CEO & Founder',
+            company: 'Standford University, example',
+            buttontext: 'Contact',
+            imageUrl: 'https://www.w3schools.com/w3images/team2.jpg',
+            phoneNumber:"7657657687"
+        },
+        {
+            name: 'David Warner',
+            title: 'CEO & Founder',
+            company: 'Sydney University, example',
+            buttontext: 'Contact',
+            imageUrl: 'https://www.w3schools.com/w3images/team3.jpg',
+            phoneNumber:"4354657576"
+        },
+        {
+            name: 'Peter Parker',
+            title: 'CEO & Founder',
+            company: 'Melbourne University, example',
+            buttontext: 'Contact',
+            imageUrl: 'https://www.w3schools.com/w3images/team4.jpg',
+            phoneNumber:"865745643543"
+        }
+]
+trio="ssaufus"
 }
