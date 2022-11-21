8d7ee98f082f429bd09218af3f8d0b35a2aea77a
karthikeyanbaburajendran <61476328+undefined@users.noreply.github.com>
2022-11-21 12:59:15 +0000

    Push from callingMethodsinParenttoChildCommunication

--- src/parentComponent.js
+++ src/parentComponent.js
@@ -1,25 +1,16 @@
-import { LightningElement,track} from 'lwc';
-export default class parentComponent extends LightningElement {
+import { LightningElement } from 'lwc';
 
-boo=false
-buttonLabel="Show Child Component"
-buttonValue="Show"
-buttonVariant="success"
+export default class MethodCaller extends LightningElement {
+  buttonAction=true
+    video = "https://www.w3schools.com/tags/movie.mp4";
 
-handleClick(event){
-this.buttonValue=event.target.value
-if(this.buttonValue=="Show"){
-  this.buttonLabel="Hide Child Component"
-  this.boo=!this.boo
-  this.buttonVariant="destructive"
-  this.buttonValue="Hide"
-}
-else{
-  this.buttonLabel="Show Child Component"
-  this.boo=!this.boo
-  this.buttonVariant="success"
-  this.buttonValue="Show"
-}
+    handlePlay() {
+        this.template.querySelector('c-child-component').play();
+        this.buttonAction=!this.buttonAction
+    }
 
-}
+    handlePause() {
+        this.template.querySelector('c-child-component').pause();
+        this.buttonAction=!this.buttonAction
+    }
 }
