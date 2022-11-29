8af6d115eb2ec11db0fa5c495b7eb45ac6095bb0
karthikeyanbaburajendran <61476328+undefined@users.noreply.github.com>
2022-11-29 14:07:04 +0000

    Push from ChildToParentViaEventBubbling

--- src/childComponent.js
+++ src/childComponent.js
@@ -2,9 +2,14 @@ import { LightningElement, api } from 'lwc';
 
 export default class VideoPlayer extends LightningElement {
 @api userDetails
-phoneNumber=false
+showDetailsofParent=false
 
-handleClick(){
-this.phoneNumber=!this.phoneNumber
+notifyParentEvent(event){
+  const bubbleVal=new CustomEvent('show',{bubbles:true}) //bubbles property shld be mentioned for bubbling to occur 
+  this.dispatchEvent(bubbleVal)
+
+}
+showHandler(){
+  this.showDetailsofParent=true
 }
 }
--- src/parentComponent.js
+++ src/parentComponent.js
@@ -1,39 +1,9 @@
 import { LightningElement,api } from 'lwc';
 
 export default class AlertParentComponent extends LightningElement {
+    showNotification=false
+showHandler(){
+    this.showNotification=true
 
- userDetails=[{
-            name: 'John Doe',
-            title: 'CEO & Founder',
-            company: 'Harvard University, example',
-            buttontext: 'Contact',
-            imageUrl: 'https://www.w3schools.com/w3images/team1.jpg',
-            phoneNumber:"9876578654"
-        },
-        {
-            name: 'Steve Smith',
-            title: 'CEO & Founder',
-            company: 'Standford University, example',
-            buttontext: 'Contact',
-            imageUrl: 'https://www.w3schools.com/w3images/team2.jpg',
-            phoneNumber:"7657657687"
-        },
-        {
-            name: 'David Warner',
-            title: 'CEO & Founder',
-            company: 'Sydney University, example',
-            buttontext: 'Contact',
-            imageUrl: 'https://www.w3schools.com/w3images/team3.jpg',
-            phoneNumber:"4354657576"
-        },
-        {
-            name: 'Peter Parker',
-            title: 'CEO & Founder',
-            company: 'Melbourne University, example',
-            buttontext: 'Contact',
-            imageUrl: 'https://www.w3schools.com/w3images/team4.jpg',
-            phoneNumber:"865745643543"
-        }
-]
-trio="ssaufus"
+}
 }
